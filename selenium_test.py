ed94bb711670a99b9c799a42c275509056fe8360
MattKovtun <griotto.ua@gmail.com>
2016-02-25 21:09:08 +0200

    to selenium_test added function connect_start() instead of flowing code.
    fixed Y coordinate of game frame, have no idea why it was working incorrect.

--- selenium_test.py
+++ selenium_test.py
@@ -1,5 +1,8 @@
+
 from selenium import webdriver
 import time
+import cv2
+
 
 driver = webdriver.Firefox()
 
@@ -10,19 +13,47 @@ el = driver.find_element_by_css_selector('#iframe-game')  # find part of the pag
 print(el.location)  # location
 print(el.size)      # size
 
-driver.save_screenshot('Jewels.png')  # save screenshot of the image to the file
+def connect_start():
+    el = driver.find_element_by_css_selector('#iframe-game')
+    #time.sleep(30)  # delay to wait while page loaded
+    for i in range(30):
+        print(30 - i, "seconds left")
+        time.sleep(1)
+    # Debug message
+    print('The page has been loaded.')
+
+    # PLAY jewel button in the middle of the screen is pressed.
+    action = webdriver.common.action_chains.ActionChains(driver)
+    action.move_to_element_with_offset(el, el.size['width'] // 2, el.size['height'] // 2)
+    action.click()
+    print(el.size['width'] // 2,el.size['height'] // 2 )
+    action.perform()
+    time.sleep(3)
+    print('click')
+    action.move_to_element_with_offset(el, 100, 65)
+    action.click()
+    action.perform()
+    time.sleep(4.5)
+
+
+connect_start()
+
 
 # ***************************************************************
 # You should cut off the image of the game from a screenshot here.
 # ***************************************************************
 
-time.sleep(60)  # delay to wait while page loaded
+driver.save_screenshot('Jewels.png')  # save screenshot of the image to the file
 
-# Debug message
-print('The page has been loaded.')
+img = cv2.imread("Jewels.png")
+crop_img = img[152:557, 125:665]
+# NOTE: its img[y: y + h, x: x + w] and *not* img[x: x + w, y: y + h]
+cv2.imwrite("cropped.png", crop_img)
+cv2.waitKey(0)
 
-# PLAY jewel button in the middle of the screen is pressed.
-action = webdriver.common.action_chains.ActionChains(driver)
-action.move_to_element_with_offset(el, el.size['width'] // 2, el.size['height'] // 2)
-action.click()
-action.perform()
+
+
+
+
+
+
