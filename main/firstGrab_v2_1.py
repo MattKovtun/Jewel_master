54fce7f95ce069da856f5c825618b5d86a635438
MattKovtun <griotto.ua@gmail.com>
2016-03-02 12:56:57 +0200

    Added correct loading. (sometimes you have to wait a few seconds more after loading is completed )

--- main/firstGrab_v2_1.py
+++ main/firstGrab_v2_1.py
@@ -11,7 +11,7 @@ import cv2
 class GameBoard:
   #  number_lst = [('number_1.jpg', '1', 0.88) ,('number_2.jpg', '2', 0.8), ('number_3.jpg', '3', 0.8) ,('number_4.jpg', '4', 0.8) , ('number_5.jpg', '5', 0.8), ('number_6.jpg', '6', 0.8), ('number_7.jpg', '7', 0.88), ('number_8.jpg', '8', 0.8), ('number_9.jpg', '9', 0.66),  ('number_0.jpg', '0', 0.8) ]
   #  new_number_lst = []
-
+    pop_cup = ('start_game.jpg', 0.66)
     gems = [('White_gem.jpg', 'white', 0.88), ('White_flame.jpg', 'white_flame', 0.88), ('White_snow.jpg', 'white_snow', 0.88), ('Blue_gem.jpg', 'blue', 0.84), ('Blue_snow.jpg', 'blue_snow', 0.82), ('Purple_gem.jpg', 'purple', 0.8), ('Purple_flame.jpg', 'purple_flame', 0.8), ('Purple_snow.jpg', 'purple_snow', 0.8), ('Yellow_gem.jpg', 'yellow', 0.82),  ('Yellow_snow.jpg', 'yellow_snow', 0.88), ('Red_gem.jpg', 'red', 0.81), ('Red_flame.jpg', 'red_flame', 0.8),  ('Red_snow.jpg', 'red_snow', 0.8), ('Orange_gem.jpg', 'orange', 0.85), ('Orange_flame.jpg', 'orange_flame', 0.85), ('Orange_snow.jpg', 'orange_snow', 0.83), ('Green_gem.jpg', 'green', 0.84), ('Green_flame.jpg', 'green_flame', 0.8), ('Green_snow.jpg', 'green_snow', 0.8), ('Blue_flame.jpg', 'blue_flame', 0.8), ('Yellow_flame.jpg', 'yellow_flame', 0.84)]
     ops = [(-1, 0), (1, 0), (0, -1), (0, 1)]
 
@@ -55,11 +55,11 @@ class GameBoard:
         self.driver.get('http://www.miniclip.com/games/bejeweled/en/')
         self.el = self.driver.find_element_by_css_selector('#iframe-game')
 
-        for i in range(30):
-            print(30 - i, "seconds left")
-            time.sleep(1)
-    # Debug message
         print('The page has been loaded.')
+        while True:
+            if new.start_game():
+                break
+
         self.action = webdriver.common.action_chains.ActionChains(self.driver)
         self.action.move_to_element_with_offset(self.el, self.el.size['width'] // 2, self.el.size['height'] // 2)
         self.action.click()
@@ -170,23 +170,47 @@ class GameBoard:
         self.action.click()
         self.action.perform()
 
+    def start_game(self):
+        self.driver.save_screenshot('Jewels.png')  # save screenshot of the image to the file
+        self.img = cv2.imread("Jewels.png")
+        self.crop_img = self.img[self.el.location['y']:self.el.location['y'] + self.el.size['height'], self.el.location['x']:self.el.location['x'] + self.el.size['width']]
+        cv2.imwrite("cropped.png"   , self.crop_img)
+        # NOTE: its img[y: y + h, x: x + w] and *not* img[x: x + w, y: y + h]
+        self.img_rgb = cv2.imread("cropped.png")
+        self.img_gray = cv2.cvtColor(self.img_rgb, cv2.COLOR_BGR2GRAY)
+        cv2.waitKey(0)
 
+        self.template = cv2.imread(GameBoard.pop_cup[0], 0)
+
+        self.res = cv2.matchTemplate(self.img_gray, self.template,cv2.TM_CCOEFF_NORMED)
+        self.threshold = GameBoard.pop_cup[1]
+        self.loc = np.where(self.res >= self.threshold)
+        self.lst = []
+
+        for self.pt in zip(*self.loc[::-1]):
+            self.lst.append(self.pt)
+        print("It's loading ,wait please...") #debug
+
+        if len(self.lst) :
+            print("Loading is completed!")
+            return True
+        else :
+            return False
 new = GameBoard()
 new.connect_start()
-new.analyze()
-#new.find_moves()
 
-gems = new.find_moves()
 
-new.swap1(gems[0][0], gems[0][1])
+
+#new.analyze()
+#gems = new.find_moves()
+#new.swap1(gems[0][0], gems[0][1])
 
 
 while True:
     try:
         time.sleep(3)
         new.analyze()
-       # new.find_moves()
-       # print(new.moves)
+
         gems = new.find_moves()
         new.swap1(gems[0][0], gems[0][1])
     except IndexError:
